b784956762a0914f69ef4357f8dd0f5a5e39a215
HAMED-cryptanalysis <98085583+HAMED-cryptanalysis@users.noreply.github.com>
2022-01-20 14:42:43 +0330

    Add files via upload

---
+++ Hardware/Implementation/Glitch/main.c
@@ -0,0 +1,20 @@
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "global.h"
+
+int main(void)
+{
+
+	sbi(DDRB,0);
+	cbi(PORTB,0);
+	sbi(DDRB,1);
+	cbi(PORTB,1);
+
+	while (TRUE)
+	{
+		sbi(PORTB,0);
+		cbi(PORTB,0);
+
+	}
+}
